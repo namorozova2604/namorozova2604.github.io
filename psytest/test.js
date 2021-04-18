ce51d9cef56d1e61839520d98e9cd7eb388e7503
Losik <namorozova2604@yandex.ru>
2021-04-18 21:05:19 +0200

    ready

---
+++ psytest/test.js
@@ -0,0 +1,264 @@
+'use strict';
+
+
+function calculateEmodepriv() {
+    let result = +(document.getElementById("emodepriv1").value) + 
+                         +(document.getElementById("emodepriv2").value) +
+                         +(document.getElementById("emodepriv3").value) +
+                         +(document.getElementById("emodepriv4").value) +
+                         +(document.getElementById("emodepriv5").value);
+
+    let resultEmodepriv = result * 4 - 20;
+    document.getElementById("resultEmodepriv").innerHTML = resultEmodepriv + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after').style.height = height;
+}
+
+function calculatePokinutost() {
+    let result = +(document.getElementById("pokinutost1").value) + 
+                         +(document.getElementById("pokinutost2").value) +
+                         +(document.getElementById("pokinutost3").value) +
+                         +(document.getElementById("pokinutost4").value) +
+                         +(document.getElementById("pokinutost5").value);
+
+    let resultPokinutost = result * 4 - 20;
+    document.getElementById("resultPokinutost").innerHTML = resultPokinutost + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after2').style.height = height;
+}
+
+function calculateNedoverie() {
+    let result = +(document.getElementById("nedoverie1").value) + 
+                         +(document.getElementById("nedoverie2").value) +
+                         +(document.getElementById("nedoverie3").value) +
+                         +(document.getElementById("nedoverie4").value) +
+                         +(document.getElementById("nedoverie5").value);
+
+    let resultNedoverie = result * 4 - 20;
+    document.getElementById("resultNedoverie").innerHTML = resultNedoverie + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after3').style.height = height;
+}
+
+function calculateOtchujd() {
+    let result = +(document.getElementById("otchujd1").value) + 
+                         +(document.getElementById("otchujd2").value) +
+                         +(document.getElementById("otchujd3").value) +
+                         +(document.getElementById("otchujd4").value) +
+                         +(document.getElementById("otchujd5").value);
+
+    let resultOtchujd = result * 4 - 20;
+    document.getElementById("resultOtchujd").innerHTML = resultOtchujd + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after4').style.height = height;
+}
+
+function calculateDefekt() {
+    let result = +(document.getElementById("defekt1").value) + 
+                         +(document.getElementById("defekt2").value) +
+                         +(document.getElementById("defekt3").value) +
+                         +(document.getElementById("defekt4").value) +
+                         +(document.getElementById("defekt5").value);
+
+    let resultDefekt = result * 4 - 20;
+    document.getElementById("resultDefekt").innerHTML = resultDefekt + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after5').style.height = height;
+}
+
+function calculateNeuspeh() {
+    let result = +(document.getElementById("neuspeh1").value) + 
+                         +(document.getElementById("neuspeh2").value) +
+                         +(document.getElementById("neuspeh3").value) +
+                         +(document.getElementById("neuspeh4").value) +
+                         +(document.getElementById("neuspeh5").value);
+
+    let resultNeuspeh = result * 4 - 20;
+    document.getElementById("resultNeuspeh").innerHTML = resultNeuspeh + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after6').style.height = height;
+}
+
+function calculateZavisimost() {
+    let result = +(document.getElementById("zavisimost1").value) + 
+                         +(document.getElementById("zavisimost2").value) +
+                         +(document.getElementById("zavisimost3").value) +
+                         +(document.getElementById("zavisimost4").value) +
+                         +(document.getElementById("zavisimost5").value);
+
+    let resultZavisimost = result * 4 - 20;
+    document.getElementById("resultZavisimost").innerHTML = resultZavisimost + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after7').style.height = height;
+}
+
+function calculateUyazvimost() {
+    let result = +(document.getElementById("uyazvimost1").value) + 
+                         +(document.getElementById("uyazvimost2").value) +
+                         +(document.getElementById("uyazvimost3").value) +
+                         +(document.getElementById("uyazvimost4").value) +
+                         +(document.getElementById("uyazvimost5").value);
+
+    let resultUyazvimost = result * 4 - 20;
+    document.getElementById("resultUyazvimost").innerHTML = resultUyazvimost + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after8').style.height = height;
+}
+
+function calculateIdent() {
+    let result = +(document.getElementById("ident1").value) + 
+                         +(document.getElementById("ident2").value) +
+                         +(document.getElementById("ident3").value) +
+                         +(document.getElementById("ident4").value) +
+                         +(document.getElementById("ident5").value);
+
+    let resultIdent = result * 4 - 20;
+    document.getElementById("resultIdent").innerHTML = resultIdent + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after9').style.height = height;
+}
+
+function calculatePokornost() {
+    let result = +(document.getElementById("pokornost1").value) + 
+                         +(document.getElementById("pokornost2").value) +
+                         +(document.getElementById("pokornost3").value) +
+                         +(document.getElementById("pokornost4").value) +
+                         +(document.getElementById("pokornost5").value);
+
+    let resultPokornost = result * 4 - 20;
+    document.getElementById("resultPokornost").innerHTML = resultPokornost + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after10').style.height = height;
+}
+
+function calculateSamopoj() {
+    let result = +(document.getElementById("samopoj1").value) + 
+                         +(document.getElementById("samopoj2").value) +
+                         +(document.getElementById("samopoj3").value) +
+                         +(document.getElementById("samopoj4").value) +
+                         +(document.getElementById("samopoj5").value);
+
+    let resultSamopoj = result * 4 - 20;
+    document.getElementById("resultSamopoj").innerHTML = resultSamopoj + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after11').style.height = height;
+}
+
+function calculatePodavl() {
+    let result = +(document.getElementById("podavl1").value) + 
+                         +(document.getElementById("podavl2").value) +
+                         +(document.getElementById("podavl3").value) +
+                         +(document.getElementById("podavl4").value) +
+                         +(document.getElementById("podavl5").value);
+
+    let resultPodavl = result * 4 - 20;
+    document.getElementById("resultPodavl").innerHTML = resultPodavl + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after12').style.height = height;
+}
+
+function calculateStand() {
+    let result = +(document.getElementById("stand1").value) + 
+                         +(document.getElementById("stand2").value) +
+                         +(document.getElementById("stand3").value) +
+                         +(document.getElementById("stand4").value) +
+                         +(document.getElementById("stand5").value);
+
+    let resultStand = result * 4 - 20;
+    document.getElementById("resultStand").innerHTML = resultStand + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after13').style.height = height;
+}
+
+function calculateGrandioz() {
+    let result = +(document.getElementById("grandioz1").value) + 
+                         +(document.getElementById("grandioz2").value) +
+                         +(document.getElementById("grandioz3").value) +
+                         +(document.getElementById("grandioz4").value) +
+                         +(document.getElementById("grandioz5").value);
+
+    let resultGrandioz = result * 4 - 20;
+    document.getElementById("resultGrandioz").innerHTML = resultGrandioz + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after14').style.height = height;
+}
+
+function calculateSamokontrol() {
+    let result = +(document.getElementById("samokontrol1").value) + 
+                         +(document.getElementById("samokontrol2").value) +
+                         +(document.getElementById("samokontrol3").value) +
+                         +(document.getElementById("samokontrol4").value) +
+                         +(document.getElementById("samokontrol5").value);
+
+    let resultSamokontrol = result * 4 - 20;
+    document.getElementById("resultSamokontrol").innerHTML = resultSamokontrol + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after15').style.height = height;
+}
+
+function calculateOdobr() {
+    let result = +(document.getElementById("odobr1").value) + 
+                         +(document.getElementById("odobr2").value) +
+                         +(document.getElementById("odobr3").value) +
+                         +(document.getElementById("odobr4").value) +
+                         +(document.getElementById("odobr5").value);
+
+    let resultOdobr = result * 4 - 20;
+    document.getElementById("resultOdobr").innerHTML = resultOdobr + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after16').style.height = height;
+}
+
+function calculateNegat() {
+    let result = +(document.getElementById("negat1").value) + 
+                         +(document.getElementById("negat2").value) +
+                         +(document.getElementById("negat3").value) +
+                         +(document.getElementById("negat4").value) +
+                         +(document.getElementById("negat5").value);
+
+    let resultNegat = result * 4 - 20;
+    document.getElementById("resultNegat").innerHTML = resultNegat + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after17').style.height = height;
+}
+
+function calculatePunit() {
+    let result = +(document.getElementById("punit1").value) + 
+                         +(document.getElementById("punit2").value) +
+                         +(document.getElementById("punit3").value) +
+                         +(document.getElementById("punit4").value) +
+                         +(document.getElementById("punit5").value);
+
+    let resultPunit = result * 4 - 20;
+    document.getElementById("resultPunit").innerHTML = resultPunit + "%";
+
+    let height = result * 10 + 'px';
+      document.getElementById('height-after18').style.height = height;
+}
+
+
+
+
+
+
+
+
+ 
+
